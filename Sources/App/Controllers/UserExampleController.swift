d7fb7e5796d1e834deb1ff87f99e46b8979a0cf2
Ferhan Akkan <ferhanakkan@gmail.com>
2020-08-18 18:42:32 +0300

    Routes are grouped

--- Sources/App/Controllers/UserExampleController.swift
+++ Sources/App/Controllers/UserExampleController.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import Vapor
+import FluentSQLite
+
+struct UserExampleController: RouteCollection {
+    
+    func boot(router: Router) throws {
+        
+        //        userRoutes.get("/", use: getAll) Hem / koyabilir yada adres eklemeden de kullanabilirsin. as =  userRoutes.get(use: getAll)
+        
+        let userRoutes = router.grouped("user")
+        userRoutes.delete(UserExampleDBModel.parameter, use: deleteSelected)
+        userRoutes.post(UserExampleDBModel.self, at: "/", use: postUser)
+        userRoutes.get(use: getAll)
+        userRoutes.get(UserExampleDBModel.parameter, use: getById)
+        userRoutes.get("nameFilter",String.parameter, use: getFiltered)
+    }
+    
+    func postUser(req:Request, user: UserExampleDBModel) throws -> Future<UserExampleDBModel> {
+        return user.save(on: req)
+    }
+    
+    func deleteSelected(req: Request) throws -> Future<UserExampleDBModel> {
+        try req.parameters.next(UserExampleDBModel.self).delete(on: req)
+    }
+    
+    func getAll(req: Request) -> Future<[UserExampleDBModel]> {
+        return UserExampleDBModel.query(on: req).all()
+    }
+    
+    
+    func getById(req:Request) throws -> Future<UserExampleDBModel> {
+        return try req.parameters.next(UserExampleDBModel.self)
+    }
+
+    func getFiltered(req: Request) throws -> Future<[UserExampleDBModel]> {
+        let name = try req.parameters.next(String.self).lowercased()
+        return UserExampleDBModel.query(on: req).filter(\.name == name).all()
+    }
+
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -17,37 +17,7 @@ struct Family: Content {
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     
-    router.get() { _ in 
-        return "Ferhan Api Test"
-    }
-    
-
-    //modeli direk tabloya kaydeder
-    router.post(UserExampleDBModel.self, at: "userModel") { req, userModel -> Future<UserExampleDBModel> in
-        return userModel.save(on: req)
-    }
-    
-    // Parametreye gore tablodan silme
-    router.delete("userModel", UserExampleDBModel.parameter) { req -> Future<UserExampleDBModel> in
-        try req.parameters.next(UserExampleDBModel.self).delete(on: req)
-    }
-    
-    //o modele ait butun tabloyu getirir
-    router.get("userModelAll") { req -> Future<[UserExampleDBModel]> in
-        return UserExampleDBModel.query(on: req).all()
-    }
-    
-    // id ile tablodan cagirma
-    //Sql modeli inherit ediypr diye idsini direk algiliyor
-    router.get("userGetById",UserExampleDBModel.parameter) { req -> Future<UserExampleDBModel> in
-        return try req.parameters.next(UserExampleDBModel.self)
-    }
-    
-    // Parametreye gore tablodan filtre ile veri al
-    router.get("getFilteredUserByName",String.parameter) { req -> Future<[UserExampleDBModel]> in
-        let name = try req.parameters.next(String.self).lowercased()
-        return UserExampleDBModel.query(on: req).filter(\.name == name).all()
-    }
-    
+    let userController = UserExampleController()
+    try router.register(collection: userController)
 
 }
